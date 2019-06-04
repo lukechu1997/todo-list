3a649d49dd93f00b03bfc92f07e3e8381377e92c
Luke Chu <lukechu421@gmail.com>
2019-06-05 01:36:54 +0800

    add vue

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+  el:'#app',
+
+  data: {
+    message: 'hi, Vue',
+    days: ['6/3','6/4']
+  //   todo: {
+  //     date: ['6/3'],
+  //     thing: ['buy things']
+  //   }
+  }
+}) 
+console.log('done')
